f020bbdfe6a4964e234970fa11341014f7625ce8
Geoffrey CRAMAY <gcramay.externe@pagesjaunes.fr>
2018-03-11 11:36:42 +0100

    Add domain layer model

---
+++ app/src/main/java/com/example/gcramay/testleboncoin/domain/model/Photo.kt
@@ -0,0 +1,12 @@
+package com.example.gcramay.testleboncoin.domain.model
+
+/**
+ * Model used to represent a photo
+ *
+ * Created on 05/03/2018
+ *
+ * @version 1.0
+ * @author gcramay
+ */
+
+data class Photo(val albumId: Int, val id: Int, val title: String, val url: String, val thumbnailUrl: String)
