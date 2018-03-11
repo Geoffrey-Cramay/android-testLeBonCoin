0ba95ead64a7fe1ae3f5ef8a721b5715a9ce46bd
Geoffrey CRAMAY <gcramay.externe@pagesjaunes.fr>
2018-03-11 11:37:10 +0100

    Add presentation layer model

---
+++ app/src/main/java/com/example/gcramay/testleboncoin/presentation/model/PhotoUiItem.kt
@@ -0,0 +1,12 @@
+package com.example.gcramay.testleboncoin.presentation.model
+
+/**
+ * //TODO : Add a class header comments
+ *
+ * Created on 05/03/2018
+ *
+ * @version 1.0
+ * @author gcramay
+ */
+
+data class PhotoUiItem(val albumId: Int, val id: Int, val title: String, val url: String, val thumbnailUrl: String)
