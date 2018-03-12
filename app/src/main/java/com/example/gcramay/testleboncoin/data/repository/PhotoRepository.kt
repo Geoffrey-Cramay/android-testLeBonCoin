b83a44e06e1d6c3e69791b3c307b422795b3ba96
Geoffrey CRAMAY <gcramay.externe@pagesjaunes.fr>
2018-03-13 00:09:02 +0100

    Optimize code

--- app/src/main/java/com/example/gcramay/testleboncoin/data/repository/PhotoRepository.kt
+++ app/src/main/java/com/example/gcramay/testleboncoin/data/repository/PhotoRepository.kt
@@ -1,11 +1,9 @@
 package com.example.gcramay.testleboncoin.data.repository
 
-import android.util.Log
-import com.example.gcramay.testleboncoin.data.local.PhotoLocalServiceImpl
+import com.example.gcramay.testleboncoin.data.local.PhotoLocalService
 import com.example.gcramay.testleboncoin.data.local.model.PhotoDataLocal
-import com.example.gcramay.testleboncoin.data.remote.PhotoServiceImpl
+import com.example.gcramay.testleboncoin.data.remote.PhotoService
 import com.example.gcramay.testleboncoin.domain.model.Photo
-import io.reactivex.Completable
 import io.reactivex.Single
 
 /**
@@ -17,16 +15,18 @@ import io.reactivex.Single
  * @author gcramay
  */
 
-class PhotoRepository(private val photoLocalServiceImpl: PhotoLocalServiceImpl, private val photoServiceImpl: PhotoServiceImpl) {
+class PhotoRepository(private val photoLocalService: PhotoLocalService, private val photoService: PhotoService) {
 
     fun getPhotos(): Single<List<Photo>> {
-        val photoListFromDatabase = photoLocalServiceImpl.getPhotos().map {
-            it.map {
-                Photo(it.albumId, it.id, it.title, it.url, it
-                        .thumbnail_url)
+        val photoListFromDatabase by lazy {
+            photoLocalService.getPhotos().map {
+                it.map {
+                    Photo(it.albumId, it.id, it.title, it.url, it
+                            .thumbnail_url)
+                }
             }
         }
-        return photoServiceImpl.getPhotos()
+        return photoService.getPhotos()
                 .map {
                     savePhotos(it.map { PhotoDataLocal(it.albumId, it.id, it.title, it.url, it.thumbnailUrl) })
                     it.map { Photo(it.albumId, it.id, it.title, it.url, it.thumbnailUrl) }
@@ -35,7 +35,7 @@ class PhotoRepository(private val photoLocalServiceImpl: PhotoLocalServiceImpl, 
         })
     }
 
-    fun savePhotos(photoList: List<PhotoDataLocal>){
-        photoLocalServiceImpl.savePhotos(photoList)
+    fun savePhotos(photoList: List<PhotoDataLocal>) {
+        photoLocalService.savePhotos(photoList)
     }
 }
